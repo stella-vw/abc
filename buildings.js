8be03c2ffe60d7b251d9375210bd57068996ac4f
stella-vw <stella.vandewater@mail.mcgill.ca>
2026-02-07 13:26:44 -0500

    buildings and moved post to models folder

---
+++ buildings.js
@@ -0,0 +1,28 @@
+const CAMPUS_BUILDINGS = [
+  { name: "McMed", lat: 45.50303299833591, lng: -73.5822537913886 },
+  { name: "Stewart Bio", lat: 45.502849207884324, lng: -73.58146749701545 },
+  { name: "Education Building", lat:  45.50458192601548, lng: -73.58026341021584 },
+  { name: "SSMU Building", lat: 45.50358826720123, lng: -73.57816746879229},
+  { name: "Armstrong Building", lat: 45.50286472277578, lng: -73.57675161581571 },
+  { name: "Bronfman Building", lat: 45.50256679409325, lng: -73.57649908654794 },
+  { name: "Law Library", lat: 45.5036150963151, lng: -73.58055393341505 },
+  { name: "McLennan Library", lat: 45.50324529223802, lng: -73.57596693118876},
+  { name: "Redpath Libraty", lat: 45.50363630524383, lng: -73.57692179755256},
+  { name: "Schulich Library", lat: 45.50506541939511, lng:-73.57535895171594}
+  { name: "Leacock Building", lat: 45.50436564852126, lng: -73.57798617432188 },
+  { name: "Arts Building", lat: 45.50529089004143, lng:-73.57768077772403},
+  { name: "MacDonald Engineering", lat: 45.50555365286042, lng: -73.57623235464658},
+  { name: "McConnell Engineering", lat: 45.505888972532595, lng: -73.57577622727597},
+  { name: "FDA", lat: 45.505222802159565, lng: -73.57544130857727},
+  { name: "Burnside", lat: 45.50474664198739, lng: -73.57494052533619},
+  { name: "Otto Maass", lat: 45.50472875795168, lng: -73.57428025702089},
+  { name: "Birks", lat: 45.50653259470649, lng: -73.57744469997559},
+  { name: "Rutherford", lat: 45.507089214594686, lng: -73.57841436937427},
+  { name: "Wong", lat: 45.50661307021689, lng: -73.57871420135216},
+  { name: "Trottier", lat: 45.507357463178636, lng: -73.57900765397916},
+  { name: "Strathcona", lat: 45.50740440654871, lng: -73.57981145885606},
+  { name: "Strathcona (Music)", lat: 45.505751710819354, lng: -73.57346745839013},
+];
+
+ 
+ 
