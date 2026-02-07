cbccb80ccf9fd9a322aed0f6d6a8c7921fe3f200
Holly Hong <holly.hong@mail.mcgill.ca>
2026-02-07 14:26:07 -0500

    added mapprovider.tsx for google maps api

---
+++ my-app/app/MapProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { APIProvider } from "@vis.gl/react-google-maps";
+
+export default function MapProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
+      {children}
+    </APIProvider>
+  );
+}
