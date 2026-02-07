c7c75fc783eab3e4b57c46ead9e7a0d38a5e17f4
Holly Hong <holly.hong@mail.mcgill.ca>
2026-02-07 18:20:13 -0500

    sidebar in progress

--- my-app/app/main/page.tsx
+++ my-app/app/main/page.tsx
@@ -1,19 +1,10 @@
-"use client";
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import CampusMap from './mapview';
 
-const MapView = () => (
-    <div style={{ width: '100vw', height: '100vh' }}>
-        <APIProvider apiKey={"AIzaSyDAGWOcRdniYbT7aVnV0WPvQMj53mk8m2Q"} 
-        onLoad={() => console.log('Maps API has loaded.')}>
-            <Map
-                defaultZoom={17}
-                defaultCenter={ { lat: 45.505074, lng: -73.577220 } }
-                onCameraChanged={ (ev: MapCameraChangedEvent) =>
-                    console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-                }>
-            </Map>
-        </APIProvider>
-    </div>
-);
-
-export default MapView;
+export default function Home() {
+  return (
+    <main style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      {/* This is your main hub that contains the map and sidebar */}
+      <CampusMap />
+    </main>
+  );
+}
